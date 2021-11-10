2e6ba2eba9bf6f113874b25c7dd41e86d6f6e508
Vitaliy <theyamshikov@yandex.ru>
2021-11-10 12:07:09 +0600

    upd

--- src/Domain/Migrations/m_2021_07_21_084512_create_currency_table.php
+++ src/Domain/Migrations/m_2021_07_21_084512_create_currency_table.php
@@ -9,14 +9,14 @@ class m_2021_07_21_084512_create_currency_table extends BaseCreateTableMigration
 {
 
     protected $tableName = 'geo_currency';
-    protected $tableComment = '';
+    protected $tableComment = 'Валюта';
 
     public function tableStructure(Blueprint $table): void
     {
         $table->integer('id')->autoIncrement()->comment('Идентификатор');
-        $table->integer('country_id')->comment('');
-        $table->string('code')->comment('');
-        $table->string('char')->comment('');
+        $table->integer('country_id')->comment('Страна');
+        $table->string('code')->comment('Международный код страны');
+        $table->string('char')->comment('Символный код');
         $table->string('title')->comment('Название');
         $table->text('description')->comment('Описание');
 
