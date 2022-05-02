5cfdd1981ae7c9152fed170574a92fdf31be68bb
Vitaliy <theyamshikov@yandex.ru>
2022-05-02 21:01:14 +0600

    upd

--- src/Domain/Enums/Rbac/GeoCurrencyPermissionEnum.php
+++ src/Domain/Enums/Rbac/GeoCurrencyPermissionEnum.php
@@ -2,7 +2,7 @@
 
 namespace ZnBundle\Geo\Domain\Enums\Rbac;
 
-use ZnCore\Base\Interfaces\GetLabelsInterface;
+use ZnCore\Contract\Enum\Interfaces\GetLabelsInterface;
 
 class GeoCurrencyPermissionEnum implements GetLabelsInterface
 {
